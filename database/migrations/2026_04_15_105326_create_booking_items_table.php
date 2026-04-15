e9f3d0aa8d1ac9c86bd2581ed2f66abbfda8ae6e
ms-dev97 <mohammed.salem701@gmail.com>
2026-04-15 21:46:52 +0300

    create tables for booking

---
+++ database/migrations/2026_04_15_105326_create_booking_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained()->onDelete('cascade');
+            $table->foreignId('service_id')->nullable()->constrained()->onDelete('set null');
+            $table->date('booking_date');
+            $table->foreignId('time_slot_id')->nullable()->constrained()->onDelete('set null');
+            $table->string('status')->default('pending'); // pending, confirmed, completed, cancelled
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_items');
+    }
+};
