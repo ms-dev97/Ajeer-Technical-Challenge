94998a3fb53d4c33dd08fee1180a07859fdad015
ms-dev97 <mohammed.salem701@gmail.com>
2026-04-15 21:54:28 +0300

    register middleware to check user active subscription

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureUserHasActiveSubscription;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,7 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'subscribed' => EnsureUserHasActiveSubscription::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
